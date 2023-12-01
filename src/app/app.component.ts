880efc7ddcc4391edbd603222dec00ceaad20acb
Dan <webdevd4n@gmail.com>
2023-12-01 23:56:54 +0000

    Created navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { RegisterComponent } from './core/register/register.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  showDropdown: any = false;
+
+  toggleShowDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+}
