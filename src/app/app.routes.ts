b3ed9863de81faa4a9cf94eb147dd7eb1eb0ba25
Dan <webdevd4n@gmail.com>
2023-12-01 22:14:50 +0000

    Added routing to login and register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent, title: 'Hive-D | Login' },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Hive-D | Register',
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
